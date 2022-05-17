09b9e7231e7b97e9a7fae3f4b3d29be17abf1152
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-05-17 15:46:56 +0200

    main page styling

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,23 +5,23 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-12 ps-4">
+            <div class="col-12 d-flex justify-content-center">
                 <h1>Comics:</h1>
-                @foreach ($comics as $comic)
-                <div class="col-3">
-                    <div class="card" style="width: 18rem;">
-                        <img src="{{ $comic->thumb }}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $comic->title }}</h5>
-                            <p class="card-text">{{ $comic->series }}</p>
-                            <p class="card-text">{{ $comic->sale_date }}</p>
-                            <p class="card-text">{{ $comic->price }}</p>
-                            <a href="#" class="btn btn-primary">Info</a>
-                        </div>
+            </div>
+            @foreach ($comics as $comic)
+            <div class="col-4 d-flex justify-content-center">
+                <div class="card mb-5" style="width: 18rem;">
+                    <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $comic->title }}</h5>
+                        <p class="card-text">{{ $comic->series }}</p>
+                        <p class="card-text">{{ $comic->sale_date }}</p>
+                        <p class="card-text">{{ $comic->price }}</p>
+                        <a href="#" class="btn btn-primary">Info</a>
                     </div>
                 </div>
-                @endforeach
             </div>
+            @endforeach
         </div>
     </div>
 @endsection
