a56fc7ae7d76fdd00dd735520a6e0147280485ec
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-05-17 14:16:29 +0200

    added basic layout and bootstrap

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Comics')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12 ps-4">
+                <h1>Comics:</h1>
+            </div>
+        </div>
+    </div>
+@endsection
